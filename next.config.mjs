f2abd2ca4b8fb61d766112102ed222a46b0f22a3
JAI <jai@fiveoak.com>
2024-07-07 12:20:17 +0530

    Update: next.config with basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
      trailingSlash: true,
+     basePath: '/raizzify',
      output: 'export',
      images: { unoptimized: true }
 };
