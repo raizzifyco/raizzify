63899a4ba2142f16b341af7d5ad8b7c39dc25e3a
jvoltci <jvoltci@gmail.com>
2025-05-31 12:49:05 +0530

    Fix: minor

--- src/styles/state/toggle-state.ts
+++ src/styles/state/toggle-state.ts
@@ -8,13 +8,16 @@ export enum Section {
 
 interface ToggleState {
   section: Section;
+  isToggled: boolean;
   changeSection: (val: Section) => void;
 }
-export const useToggle = create<ToggleState>((set) => {
-  return {
-    section: Section.partner,
-    changeSection: (val: Section) => {
-      set({ section: val });
-    },
-  };
-});
+
+export const useToggle = create<ToggleState>((set, get) => ({
+  section: Section.partner,
+  get isToggled() {
+    return get().section !== Section.partner;
+  },
+  changeSection: (val: Section) => {
+    set({ section: val });
+  },
+}));
