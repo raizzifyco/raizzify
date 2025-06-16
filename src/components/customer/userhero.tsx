8a800f6f602b5b83eb2803fc6f5d41ae55741abb
108Ashish <ashish.singhsuttle@gmail.com>
2025-06-16 20:23:29 +0530

    updata

--- src/components/customer/userhero.tsx
+++ src/components/customer/userhero.tsx
@@ -1,11 +1,12 @@
 "use client";
-import 'animate.css';
+
 import { useEffect, useMemo, useState } from "react";
 import Image from "next/image";
 import Hero5 from "../../../public/images/user1/img1.svg";
 import Hero6 from "../../../public/images/user1/appstore.svg";
 import Hero7 from "../../../public/images/user1/google.svg";
 import Switch from "../switch";
+//import 'animate.css';
 
 const UserHero = () => {
   // UseMemo prevents warnings and unnecessary re-creation
@@ -79,7 +80,7 @@ const UserHero = () => {
         </div>
 
         <div className="absolute top-4 w-full flex justify-center">
-          <div className="animate__animated animate__shakeY animate__repeat-2 text-center text-[#416660] text-sm md:text-lg font-extrabold">
+          <div className=" text-center text-[#416660] text-sm md:text-lg font-extrabold">
             By Team of IITians and ISBians
           </div>
         </div>
